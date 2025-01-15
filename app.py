e8667adbce40108fa37f2b6806d5c8108e724fcb
kousalyamd11 <kousalya.md@brandsystems.com>
2025-01-15 09:56:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,14 +45,14 @@ app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
 # Add these configurations
 DB_NAME = 'image_metadata.db'
 model = SentenceTransformer('all-MiniLM-L6-v2')
-secret_key = <your-secret-key>  # 32 bytes = 64 characters
-app.config['SECRET_KEY'] = 'your secrete key'  # Change this to a secure secret key
+secret_key = secrets.token_hex(32) # 32 bytes = 64 characters
+app.config['SECRET_KEY'] = 'secret_key' 
 JWT_EXPIRATION_DELTA = timedelta(days=7)
 app.config['SMTP_SERVER'] = 'replace with your smtp server'  # Replace with your SMTP server
 app.config['SMTP_PORT'] = 'replace with your smtp port'
 app.config['SMTP_USERNAME'] = 'example@gmail.com'  # Replace with your email
 app.config['SMTP_PASSWORD'] = 'google app password'  # Replace with your email password
-RESET_CODES = {}  # Store reset codes temporarily (consider using Redis in production)
+RESET_CODES = {} 
 
 # Define the token_required decorator
 def token_required(f):
